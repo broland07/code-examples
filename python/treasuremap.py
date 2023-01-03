08df45dc7cdbf16e341ad686fcc3487c5cec65b9
Balla Roland <balla.roland96@gmail.com>
2023-01-03 20:16:45 +0200

    fix var

--- python/treasuremap.py
+++ python/treasuremap.py
@@ -5,7 +5,6 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-marker = "X"
 horizontal = int(position[0])
 vertical = int(position[1])
 map[ vertical -1][horizontal -1] = "X"
