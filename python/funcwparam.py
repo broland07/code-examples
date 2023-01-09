7a731abe787ec9bdff0f2b80d70c6b12cc7623b5
Balla Roland <balla.roland96@gmail.com>
2023-01-09 20:35:14 +0200

    c.e.

---
+++ python/funcwparam.py
@@ -0,0 +1,11 @@
+def greet():
+  print("Hello")
+  print("How do you do?")
+
+greet()
+
+def greet_w_name(name):
+  print(f"Hello, {name}!")
+  print(f"How do you do, {name}?")
+
+greet_w_name("Jack")
