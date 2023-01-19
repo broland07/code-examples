b601c3b312d113ac1227f68ead398968432fec16
Balla Roland <balla.roland96@gmail.com>
2023-01-19 20:58:23 +0200

    c.e.

--- python/nesting.py
+++ python/nesting.py
@@ -1,5 +1,3 @@
-##Python Dictionaries
-
 programming_dictionary = {
   "Bug": "An error in a program that prevents the program from running as expected.", 
   "Function": "A piece of code that you can easily call over and over again.",
