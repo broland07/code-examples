ba47a93106e449d08ed08882424adc408ba31908
Balla Roland <balla.roland96@gmail.com>
2023-01-21 19:00:41 +0200

    c.e.

---
+++ python/dict3.py
@@ -0,0 +1,18 @@
+import os
+
+dict = {}
+end = False
+
+while not end:
+    key = input("Key: ")
+    value = input("Value: ")
+    dict[key] = value
+    end_of = input("Do you wanna add new value? Yes/No: ")
+    if end_of == "Yes" or end_of == "yes":
+        os.system('clear')
+    elif end_of == "No" or end_of == "no":
+        end = True
+
+for elements in dict:
+    get_value = dict[elements]
+    print(f"The key is: {elements}, and the value is: {get_value}.")
