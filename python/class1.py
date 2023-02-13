fe0fc49a348e0bd4363fff5c0009c04d5ec1a897
Balla Roland <balla.roland96@gmail.com>
2023-02-13 17:44:03 +0200

    c.e.

--- python/class1.py
+++ python/class1.py
@@ -4,7 +4,13 @@ class User:
         self.id = user_id
         self.username = username
         self.followers = 0
-#        print("New user being created...")
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
 
 user_1 = User("001", "roland")
 user_2 = User("002", "jack")
@@ -14,3 +20,9 @@ print(user_1.username)
 print(user_2.username)
 
 print(user_1.followers)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
