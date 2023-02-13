a4f713201659e2f6126422f2493e9de05fe710f2
Balla Roland <balla.roland96@gmail.com>
2023-02-13 20:29:47 +0200

    c.e.

---
+++ python/project3/main.py
@@ -0,0 +1,14 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for elements in question_data:
+    question_text = elements["text"]
+    question_answer = elements["answer"]
+    new_question = Question(q_text=question_text, q_answer=question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
