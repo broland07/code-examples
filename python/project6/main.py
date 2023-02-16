c97d79c68fb7f944e1bcd198f761940aee0cec3e
Balla Roland <balla.roland96@gmail.com>
2023-02-16 21:03:57 +0200

    c.e.

--- python/project6/main.py
+++ python/project6/main.py
@@ -18,4 +18,12 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))
+# print(data["temp"])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list =  data["temp"].to_list()
+average = sum(temp_list)/len(temp_list)
+print(round(average))
