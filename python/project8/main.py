fef80df9b8f969941e465f16bf1e271c70a8d38c
Balla Roland <balla.roland96@gmail.com>
2023-02-28 19:43:58 +0200

    c.e.

--- python/project8/main.py
+++ python/project8/main.py
@@ -1,5 +1,7 @@
 import requests
 
 respons = requests.get(url="http://api.open-notify.org/iss-now.json")
+respons.raise_for_status()
 
-print(respons)
+data = respons.json()["iss_position"]
+print(data)
