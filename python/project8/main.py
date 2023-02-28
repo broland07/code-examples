6cec221abb27ebec9c6664e63a0dd4d914a95a66
Balla Roland <balla.roland96@gmail.com>
2023-02-28 19:55:25 +0200

    c.e.

--- python/project8/main.py
+++ python/project8/main.py
@@ -1,7 +1,20 @@
 import requests
 
-respons = requests.get(url="http://api.open-notify.org/iss-now.json")
-respons.raise_for_status()
+# respons = requests.get(url="http://api.open-notify.org/iss-now.json")
+# respons.raise_for_status()
 
-data = respons.json()["iss_position"]
-print(data)
+# data = respons.json()["iss_position"]
+# print(data)
+
+kanye = requests.get(url="https://api.kanye.rest")
+data = kanye.json()["quote"]
+
+new_quotes = {}
+
+i = 0
+
+while i <= 10:
+   new_quotes[i]=data
+   i += 1
+
+print(new_quotes)
