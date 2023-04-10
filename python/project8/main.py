71f4f871b5c5b61283ed629eee785e3945da6d8c
Balla Roland <balla.roland96@gmail.com>
2023-04-10 12:31:09 +0300

    c.e.

--- python/project8/main.py
+++ python/project8/main.py
@@ -9,12 +9,4 @@ import requests
 kanye = requests.get(url="https://api.kanye.rest")
 data = kanye.json()["quote"]
 
-new_quotes = {}
-
-i = 0
-
-while i <= 10:
-   new_quotes[i]=data
-   i += 1
-
-print(new_quotes)
+print(data)
